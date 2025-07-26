fd95564f10d546180ba4e64c3dd05c1b87976e55
djskin1 <hckevin1990@live.nl>
2025-07-26 18:00:27 +0200

    1ste start plugin

---
+++ Rooziesplugin.java
@@ -0,0 +1,18 @@
+package com.roozies;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public  class Rooziesplugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+        getLogger().info("Roozies plugin has been enabled!");
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+        getLogger().info("Roozies plugin has been disabled!");
+    }
+}
